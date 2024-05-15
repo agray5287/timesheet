70e0dc21220ece1df77816112c950fd2a2acbc30
Andy Gray <agray5287@gmail.com>
2024-05-15 17:00:04 -0500

    Prettier table

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,14 +1,15 @@
 export function HomePage() {
   return (
     <div class="grid grid-cols-2">
-      <div class="flex justify-center p-5">
-        <table class="">
-          <tr class="">
-            <th class="border w-20">Day</th>
-            <th class="border w-60">Project</th>
-            <th class="border w-20">Hours</th>
-            <th class="border w-60">Comment</th>
-          </tr>
+      <div class="flex justify-center p-5 overflow-x-auto">
+        <table class="table-auto w-full text-sm text-left text-gray-200" id="mainTable">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+            <th class="border w-20 px-6 py-3 cursor-pointer" onclick="sortTable(0, 'mainTable')">Day</th>
+            <th class="border w-60 px-6 py-3 cursor-pointer" onclick="sortTable(1, 'mainTable')">Project</th>
+            <th class="border w-20 px-6 py-3 cursor-pointer" onclick="sortTable(2, 'mainTable')">Hours</th>
+            <th class="border w-60 px-6 py-3 cursor-pointer" onclick="sortTable(3, 'mainTable')">Comment</th>
+          </thead>
+          <tbody class="">
           <tr class="">
             <td class="border text-center">Mon</td>
             <td class="border">Northern Beltline</td>
@@ -33,9 +34,10 @@ export function HomePage() {
             <td class="border text-center">8</td>
             <td class="border">Also fixing corrupted files</td>
           </tr>
+          </tbody>
         </table>
       </div>
-      <div class="bg-yellow-900 flex justify-center p-5">
+      <div class=" flex justify-center p-5">
         <table>
           <tr>
             <th class="col-span-2 text-center">Weekly Hours</th>
